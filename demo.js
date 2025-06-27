48391aad1ca8fd123d18784ce73e10fa05b0b558
Aiman Munawar <aimanmunawar6@gmail.com>
2025-06-27 23:11:38 +0500

    1

---
+++ demo.js
@@ -0,0 +1,10 @@
+
+// push()  ===> enqueue
+// shift() ===> Dequeue
+// arr[0]  ===> peak
+// isEmpty ===> function write kerna pry ga.
+
+// push
+// pop
+// arr[arr.length-1]
+// isEmpty ====> function
