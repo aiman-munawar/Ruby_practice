34bbbb3c80750fcbbeb71d50c22bd29e39fb1489
Aiman Munawar <aimanmunawar6@gmail.com>
2025-06-27 23:16:24 +0500

    10

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+
+# datatype runtype pe decide datatype
+a = 10 
+# print(type(a)) # here a is int
+
+a = 'farooq'
+# print(type(a)) # here a is str
+
+
+name = 'farooq'
+
+avg = 3.7
+
+num = 20
+
+isYoung = True
+isChild = False
+
+
+
+num = '2'  # here num is str
+
+num2=int(num)  # here num2 is int
+
+
+num3=float(num2) # here num3 is float
+
+my_num = 2
+
+my_num_str = str(my_num) # here my_num_str is str
+
+
+
+num = bool(input('Enter A Bool:')) # input de str
+
+print(type(num))
